0a067f43552c9dea82f17ff897a5bbf197165a81
Tim Anderson <tdanderson805@gmail.com>
2021-01-24 12:45:40 -0800

    advanced content mostly complete

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,5 +1,10 @@
 console.log('running Tic Tac Toe')
-var move = 1
+var move = 0
+var tieTotalCount = 9;
+var playCount = 0
+var playerXtotal = 0;
+var playerYtotal = 0;
+var play = 'X'
 
 
 //have two sets of winner buckets
@@ -27,9 +32,27 @@ var yMoves = {
 }
 
 
-var resetGame = function () {
-  console.log('in the reset game function')
-  move = 1
+var resetGame = function (tieVal, hardReset) {
+  // console.log('in the reset game function')
+  if (tieVal === 9) {
+    move = 0
+  } else if (tieVal === 10) {
+    move = 1;
+  }
+  //reset the window message
+  if (hardReset) {
+    document.getElementById('windowStatus').innerText = "Player X Goes First";
+    move = 0;
+    tieTotalCount = 8;
+    playerXtotal = 0;
+    playerYtotal = 0;
+    playCount = 0
+    document.getElementById('Xtotal').innerText = `Win Total: ${playerXtotal}`;
+    document.getElementById('Ytotal').innerText = `Win Total: ${playerYtotal}`;
+    document.getElementById('playCount').innerText = `Total Play Count: 0`;
+  }
+
+  //reset counters
   var playBlocks = document.getElementsByClassName("playSquare");
   [].slice.call( playBlocks ).forEach(function ( block ) {
     block.innerHTML = '';
@@ -46,9 +69,9 @@ var resetGame = function () {
 
 
 var playHandler = function (ID, Player) {
-  console.log(xMoves);
-  console.log('logging the top row below')
-  console.log(xMoves['topRow'])
+  // console.log(xMoves);
+  // console.log('logging the top row below')
+  // console.log(xMoves['topRow'])
 
 
   if ((ID === '1A') && (Player === 'X')) {
@@ -120,35 +143,58 @@ var playHandler = function (ID, Player) {
   }
 
 
-  console.log(xMoves)
-  console.log(yMoves)
+  // console.log(xMoves)
+  // console.log(yMoves)
 
 
 
-  //check for a winner
+  //check for X winner
   for (var key in xMoves) {
     if (xMoves[key] === 3) {
       resetGame();
-      alert("Player X won the game");
+      playCount++
+      move = 0
+      tieTotalCount = 9;
+      playerXtotal++;
+      let playerXName = document.getElementById('player1').value;
+      console.log(playerXName);
+      document.getElementById('windowStatus').innerText = `Game Over !!!`;
+      setTimeout(()=>{document.getElementById('windowStatus').innerText = `${playerXName} won, X goes first`}, 750);
+      document.getElementById('Xtotal').innerText = `Win Total: ${playerXtotal}`;
+      document.getElementById('playCount').innerText = `Total Play Count: ${playCount}`;
     }
   }
+
+  //check for Y winner
   for (var key in yMoves) {
     if (yMoves[key] === 3) {
       resetGame();
-      alert("Player Y won the game");
+      playCount++
+      move = 1
+      tieTotalCount = 10;
+      playerYtotal++;
+      let playerYName = document.getElementById('player2').value;
+      // console.log(playerYName);
+      document.getElementById('windowStatus').innerText = `Game Over !!!`;
+      setTimeout(()=>{document.getElementById('windowStatus').innerText = `${playerYName} won, O goes first`}, 750);
+      document.getElementById('Ytotal').innerText = `Win Total: ${playerYtotal}`;
+      document.getElementById('playCount').innerText = `Total Play Count: ${playCount}`;
     }
   }
   //if moves = 9 then the game is over
-  if (move === 9) {
-    resetGame();
-    alert("The game is a draw");
+  if (move === tieTotalCount) {
+    resetGame(tieTotalCount);
+    playCount++
+    document.getElementById('windowStatus').innerText = `Tie game detected, reseting now`;
+    setTimeout(()=>{document.getElementById('windowStatus').innerText = `Previous winner goes first`}, 750);
+    document.getElementById('playCount').innerText = `Total Play Count: ${playCount}`;
   }
 }
 
 //if reset button is clicked then reset the game
 document.getElementById('theButton').addEventListener("click", function () {
   console.log('reseting the game')
-  resetGame();
+  resetGame(null, true);
 })
 
 
@@ -157,17 +203,17 @@ document.getElementById('theButton').addEventListener("click", function () {
 var elements = document.getElementsByClassName("playSquare")
 for (var i = 0; i < elements.length; i++) {
   elements[i].addEventListener("click", function (event) {
-    console.log('clicked!')
-    console.log(event.target.id);
+    // console.log('clicked!')
+    // console.log(event.target.id);
     var idClickedOn = event.target.id
+    move++
+    console.log(move)
 
-    if ((move % 2)) {
+    if (move % 2) {
       document.getElementById(idClickedOn).innerHTML = 'X';
-      move++
       playHandler(idClickedOn, 'X')
     } else {
       document.getElementById(idClickedOn).innerHTML = 'O';
-      move++
       playHandler(idClickedOn, 'O')
     }
   })
